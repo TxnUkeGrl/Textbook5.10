d717cd4ae3f7425977e7766c6f4750e7b7c8d643
TxnUkeGrl <txnukegrl@gmail.com>
2021-04-25 13:30:04 -0400

    first commit

---
+++ 3.2Exercises37-38/3.2Exercises37-38/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Linq;
+
+namespace _3._2Exercises37_38
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool loop = true;
+
+            while (loop)
+            {
+                int[] numbers = new int[5];
+
+                int sum = 0;
+
+                Console.WriteLine("Give me a number!");
+
+                for (int i = 0; i < numbers.Length; i++)
+                {
+                    numbers[i] = int.Parse(Console.ReadLine());
+                    sum += numbers[i];
+
+                    if (i < numbers.Length - 1)
+                    {
+                        Console.WriteLine("\nGive me another number!");
+                    }
+                }
+
+                double average = (double)sum / numbers.Length;
+                Console.WriteLine($"\nThe sum of your array is {sum}, with an average of {average}.");
+
+                Console.WriteLine("\nWant to try a different array? Y/N");
+                string again = Console.ReadLine().ToLower();
+
+                if (again == "y")
+                {
+                    loop = true;
+                }
+                else
+                {
+                    loop = false;
+                }
+            }
+        }
+    }
+}
